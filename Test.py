8991af54f9b88c38056b054d994a7b329ed40e16
Zazased <ceyaroslav@yandex.ru>
2024-05-28 18:56:36 +0300

    tasks files

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print(1)
+# with open('random.txt', 'w') as file:
+#     user_input = input('Введите текст ')
+#     file.write(user_input)
+
+
+# with open('28.05.2024.txt','w') as file:
+#     file.write('Hello world')    
+
+
+my_list = ['apple', 'banana', 'orange', 'grape']
+with open('list_random.txt','w') as file:
+    for i in my_list:
+        file.write(f'{i}\n')
